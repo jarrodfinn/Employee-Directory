f3bf3bea922a1ca9b0e417a099419d63f415c205
Jarrod Finn <jarrod.p.finn@gmail.com>
2020-04-09 14:52:29 -0400

    updated dataArea.js sorting

--- src/components/dataArea.js
+++ src/components/dataArea.js
@@ -4,70 +4,99 @@
 
 import React, { Component } from "react";
 import App from "../App";
+import Moment from "moment";
 
 export default class DataArea extends Component {
   state = {
     //componentDidMount
     employees: [],
+    allEmployees: [],
+    isLoading: true,
     order: "descend",
     filteredUsers: [{}],
   };
   headings = ["name..."];
 
   componentDidMount() {
-    //page loads, fetch "employees" from json, set state of users and filteredusers to the result
     fetch("/employees.json")
       .then(function (response) {
         return response.json();
       })
       .then((response) => {
-        this.setState({ employees: response });
+        this.setState({ employees: response, allEmployees: response, isLoading: false });
       })
       .catch((error) => {
         console.log(error);
       });
   }
-  handleSearchChange() {
-    //filter out users
-  }
 
-  handleSort(heading) {
-    if (this.state.order === "descend") {
-      this.setState({
-        order: "ascend",
-      });
+  handlePositionChange = (event) => {
+    const position = event.target.value;
+    if (position === "All") {
+      this.setState({ employees: this.state.allEmployees });
     } else {
       this.setState({
-        order: "descend",
+        employees: this.state.allEmployees.filter(function (employee) {
+          if (employee.position === position) {
+            return true;
+          }
+          return false;
+        }),
       });
     }
-  }
+  };
+
+  sortByDateOfBirth = () => {
+    this.setState({
+      employees: this.state.employees.sort(function (a, b) {
+        var dateA = new Date(a.date).getTime();
+        var dateB = new Date(b.date).getTime();
+        return dateA > dateB ? 1 : -1;
+      })
+    });
+}
+
   render() {
+    if (this.state.isLoading === true) {
+      return <div>Loading...</div>;
+    }
     return (
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Position</th>
-            <th>Date of Birth</th>
-            <th>Email</th>
-            <th>Phone</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.employees.map(function (employee) {
-            return (
-              <tr key={employee.id}>
-                <td>{employee.name}</td>
-                <td>{employee.position}</td>
-                <td>{employee.dob}</td>
-                <td>{employee.email}</td>
-                <td>{employee.phone}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div>
+        <label for="position">Filter by position:</label>
+        <select onChange={this.handlePositionChange} id="position">
+          <option value="All">All</option>
+          <option value="Manager">Manager</option>
+          <option value="Sales">Sales</option>
+          <option value="Logistics">Logistics</option>
+          <option value="Accounting">Accounting</option>
+          <option value="Tech Support">Tech Support</option>
+        </select>
+        <button type='button' onChange={this.sortByDateOfBirth}>Filter by Date of Birth</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Position</th>
+              <th>Date of Birth</th>
+              <th>Email</th>
+              <th>Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(function (employee) {
+              return (
+                <tr key={employee.id}>
+                  <td>{employee.name}</td>
+                  <td>{employee.position}</td>
+                  <td>{employee.dob}</td>
+                  <td>{employee.email}</td>
+                  <td>{employee.phone}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,3 +1,11 @@
 import React from "react";
+function Nav() {
+  return (
+    <div className="Nav">
+      <h1>Employee Directory</h1>
+    </div>
+  );
+}
 
 //render searchbox here
+export default Nav;
